5954e44f04737bad495fc602b758b6e5099d9cf4
tarakc02 <tarakc02@users.noreply.github.com>
2018-05-04 02:18:07 +0100

    keep cdw metadata up-to-date

--- r/5a-update-cads-metadata.R
+++ r/5a-update-cads-metadata.R
@@ -18,3 +18,52 @@ res <- ROracle::dbWriteTable(
     schema = 'RDATA',
     overwrite = FALSE, append = TRUE)
 ROracle::dbCommit(cdw)
+
+### 
+# same with parties:
+cads_parties <- getcdw::get_cdw('
+select * from rdata.fec_cmte_party
+')
+
+## check
+parties %>% select(party_code, party) %>% 
+    anti_join(cads_parties, by = 'party_code')
+
+#####
+fec_candidates <- unzipper(fec_year, "cn")
+cads_candidates <- getcdw::get_cdw(
+    paste0("select * from rdata.fec_candidates where fec_cycle = ", fec_year)    
+)
+new_cads_candidates <- fec_candidates %>%
+    mutate(fec_cycle = fec_year, cand_election_yr = as.integer(cand_election_yr)) %>% 
+    anti_join(cads_candidates, by = c("fec_cycle", "cand_id")) %>%
+    select(fec_cycle, cand_id, cand_name, cand_pty_affiliation, cand_election_yr,
+           cand_office_st, cand_office, cand_office_district, cand_ici,
+           cand_status, cand_pcc)
+
+cdw <- getcdw::connect("URELUAT_DEVEL")
+res <- ROracle::dbWriteTable(
+    cdw, "FEC_CANDIDATES", new_cads_candidates, 
+    schema = 'RDATA',
+    overwrite = FALSE, append = TRUE)
+ROracle::dbCommit(cdw)
+###
+
+fec_ccl <- unzipper(fec_year, "ccl")
+cads_ccl <- getcdw::get_cdw(
+    paste0("select * from rdata.fec_ccl where fec_cycle = ", fec_year)    
+)
+
+new_cads_ccl <- fec_ccl %>% 
+    mutate(fec_cycle = fec_year,
+           linkage_id = as.integer(linkage_id),
+           cand_election_yr = as.integer(cand_election_yr),
+           fec_election_yr = as.integer(fec_election_yr)) %>% 
+    anti_join(cads_ccl, by = c("fec_cycle", "linkage_id")) %>% 
+    select(fec_cycle, everything())
+cdw <- getcdw::connect("URELUAT_DEVEL")
+res <- ROracle::dbWriteTable(
+    cdw, "FEC_CCL", new_cads_ccl, 
+    schema = 'RDATA',
+    overwrite = FALSE, append = TRUE)
+ROracle::dbCommit(cdw)
